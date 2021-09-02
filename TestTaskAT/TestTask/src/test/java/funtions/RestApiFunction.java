417a3879f9c4ded6cf793aa3129b7043fab7bfc3
Utishkalieva <autishkalieva@neoflex.ru>
2021-09-02 16:19:02 +0400

    Реализован тест-кейс тестирования REST API

--- TestTaskAT/TestTask/src/test/java/funtions/RestApiFunction.java
+++ TestTaskAT/TestTask/src/test/java/funtions/RestApiFunction.java
@@ -1,53 +1,80 @@
 package funtions;
 
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
+import testPlane.CheckRestApi;
 import utils.DataTest;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import static io.restassured.RestAssured.given;
 
 public class RestApiFunction {
+
+    Response response;
+    private final static Logger logger = Logger.getLogger(CheckRestApi.class);
     DataTest dataTest = new DataTest();
 
-    public void restApiSetUp(String uri){
-        RestAssured.baseURI = uri;
+    /////////////////////////////////////////////////////
+    public void checkRestApi(){
+        try{
+            String filmsListUrl = getRootEntities("https://swapi.dev/api/");
+            String filmUrl = getListFilms(filmsListUrl).get(0);
+            getInfo(filmUrl);
+            String planetUrl = getListEntities(filmUrl, "planets").get(0);
+            getInfo(planetUrl);
+            String speciesUrl = getListEntities(filmUrl, "species").get(0);
+            getInfo(speciesUrl);
+            List<String> starshipsList = getListEntities(filmUrl, "starships");
+            String starshipsUrl = starshipsList.get(starshipsList.size()-1);
+            getInfo(starshipsUrl);
+            getListEntities(starshipsUrl, "pilots");
+        }catch (Exception ex){
+            logger.debug(ex);
+        }
     }
+    ////////////////////////////////////////////////////
 
-    public void getListEntity(Response response, Logger logger) {
-        response = given().relaxedHTTPSValidation().contentType(ContentType.JSON).get();
+    private void getInfo(String basePath) {
 
-        Assert.assertEquals(response.statusCode(), 200);
-        logger.info(response.prettyPrint());
-    }
-
-    private String getListFilmsTest(Response response, Logger logger) {
-
-        response = given().relaxedHTTPSValidation().contentType(ContentType.JSON).get("films/");
+        response = given().relaxedHTTPSValidation().contentType(ContentType.JSON).get(basePath);
 
         Assert.assertEquals(response.statusCode(), 200);
 
         JsonPath path = response.jsonPath();
-        String nameFilm = "";
-        String url = "";
+        Map<String, String> info = path.getMap("$");
+        logger.info("Информация: " + info);
+    }
+
+    private String getRootEntities(String basePath){
+        //получить список сущностей
+        response = given().relaxedHTTPSValidation().contentType(ContentType.JSON).get(basePath);
+        Assert.assertEquals(response.statusCode(), 200);
+        //получить ссылку
+        JsonPath path = response.jsonPath();
+        Map<String, String> listEntities = path.getMap("$");
+        Set<String> listKey = listEntities.keySet();
+        String url = path.get("films");
+        logger.info("Список сущностей: " + listKey);
+        logger.info("Ссылка на список фильмов: " + url);
+        return url;
+    }
+
+    private List<String> getListFilms(String basePath) {
+        response = given().relaxedHTTPSValidation().contentType(ContentType.JSON).get(basePath);
+
+        Assert.assertEquals(response.statusCode(), 200);
+        JsonPath path = response.jsonPath();
+        List<String> url = new ArrayList<>();
         List<HashMap<String, Object>> results = path.getList("results");
         try{
             for (HashMap<String, Object> film : results) {
-                String title= (String) film.get("title");
-                logger.info(title);
-
-                if(title.equals(dataTest.FILM)){
-                    nameFilm=title;
-                    url = (String) film.get("url");
-                }
+                url.add((String) film.get("url"));
             }
-            logger.info("Был выбран фильм \""+nameFilm+"\"");
+            logger.info("Список фильмов: " + url);
         }catch (Exception ex){
             logger.debug(ex);
         }
@@ -55,12 +82,14 @@ public class RestApiFunction {
         return url;
     }
 
-    public void getFilmInfoTest(Response response, Logger logger) {
-
-        response = given().relaxedHTTPSValidation().contentType(ContentType.JSON).get(getListFilmsTest(response, logger));
-
+    private List<String> getListEntities(String basePath, String node){
+        //получить список сущностей
+        response = given().relaxedHTTPSValidation().contentType(ContentType.JSON).get(basePath);
         Assert.assertEquals(response.statusCode(), 200);
-
-        logger.info(response.prettyPrint());
+        //получить ссылку
+        JsonPath path = response.jsonPath();
+        List<String> listPlanets = path.getList(node);
+        logger.info("Список " + node + ": " + listPlanets);
+        return listPlanets;
     }
 }
--- TestTaskAT/TestTask/src/test/java/testPlane/CheckRestApi.java
+++ TestTaskAT/TestTask/src/test/java/testPlane/CheckRestApi.java
@@ -9,22 +9,10 @@ import org.testng.annotations.Test;
 
 public class CheckRestApi {
 
-    Response response;
     RestApiFunction restApiFunction = new RestApiFunction();
-    private final static Logger logger = Logger.getLogger(CheckRestApi.class);
-
-    @BeforeClass
-    public void setUp(){
-        restApiFunction.restApiSetUp("https://swapi.dev/api/");
-    }
 
     @Test(description = "GET")
     public void getListEntityTest() {
-       restApiFunction.getListEntity(response, logger);
-    }
-
-    @Test(description = "GET")
-    public void getListFilmsTest() {
-        restApiFunction.getFilmInfoTest(response, logger);
+        restApiFunction.checkRestApi();
     }
 }
